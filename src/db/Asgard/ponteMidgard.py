00c66ecd796468b97bb1fee326d333c354a83317
JeffersonDIG <jefferson8841@gmail.com>
2020-05-15 13:09:16 -0300

    add arquivos gerais

---
+++ src/db/Asgard/ponteMidgard.py
@@ -0,0 +1,30 @@
+"""
+Data........: 2020-05-14
+Projeto.....: RagnarokProject - Pizzaria
+Arquivo.....: ponteMidgard.py
+Descrição...: Conexão generalizada para toda a aplicação
+Autor.......: Jefferson de Lima
+Observações.: 2020-05-14 - [R00] Criação do Arquivo - Versao 1.00
+              ...
+Referencias:
+"""
+
+
+import os
+import sqlite3
+
+# definindo um arquivo para clientes
+fileDB = 'C:\\Users\jeffe\git\PIZZARIA-RAGNAROK\src\db\cliente.sqlite'
+
+# verificando se arquivo de banco de dados existe
+print(f'Verificando se arquivo {fileDB} existe.')
+if not os.path.exists(fileDB):
+    print(f'O arquivo: {fileDB} não existe!')
+    exit(-1)
+else:
+    pass
+
+    # Criando a base de dados
+connection = sqlite3.connect(fileDB)
+
+# Get a cursor object
---
+++ src/db/exemploSelect.py
@@ -0,0 +1,29 @@
+# """
+# Data........: 2020-05-15
+# Projeto.....: Pizzaria-Ragnarok
+# Arquivo.....: exemploSelect.py
+# Descrição...: Exemplo de consulta utilizando a ponte midgard fazendo a consulta de dados na tabela cliente de todos regitros
+# Autor.......: Jefferson de Lima - Eq. Ragnarok
+# Observações.: 2020-05-08 - [R00] Criação do Arquivo - Versao 1.00
+#
+# Referencias:
+# """
+
+
+from Asgard import ponteMidgard
+
+cursor = ponteMidgard.connection.cursor()
+cursor.execute("SELECT id, nome, cidade, salario from cliente")
+user1 = cursor.fetchone()  # retrieve the first row
+print('\nImpressao de campo a campo')
+print('Id:', user1[0])  # Imprime o primeiro campo
+print('Nome:', user1[1])  # Imprime o segundo campo
+print('Cidade:', user1[2])  # Imprime o terceiro campo
+print('Salario:', user1[3])  # Imprime o quarto campo
+
+print('\nImpressao de toda as tuplas')
+all_rows = cursor.fetchall()
+for row in all_rows:
+    # row[0] retorna a primeira coluna da query (id),
+    # row[1] retorna a segunda coluna (nome)
+    print('{0}, {1}, {2}, {3}'.format(row[0], row[1], row[2], row[3]))
