d9d92ef7f5b5b2f8887bc3153c6f626c52ed6e7a
jeffersonLim <killover8841@gmail.com>
2020-05-23 18:48:06 -0300

    subindo dados

--- src/lib/Clientes/cadastroClientes.py
+++ src/lib/Clientes/cadastroClientes.py
@@ -19,10 +19,10 @@ cursor = Bifrost.connection.cursor()
 
 def cad_cliente():  # função que cadastra as pizzas do cardápio
 
-    lista_clientes = [( '1','1', 'Paula', 'Rua Anfibólios', '562', NULL, 'Bonfim','Belo Horizonte','MG'),
-                   ( '2','2', 'Camila', 'Parque Anhangabaú', '82', NULL, 'Centro', 'São Paulo', 'SP'),
+    lista_clientes = [( '1','1', 'Paula', 'Rua Anfibólios', '562', 'NULL', 'Bonfim','Belo Horizonte','MG'),
+                   ( '2','2', 'Camila', 'Parque Anhangabaú', '82', 'NULL', 'Centro', 'São Paulo', 'SP'),
                    ( '3','3', 'Jefferson', 'Rua Condado', '585', 'Ap 76 BL 5', 'Cavalhada','Porto Alegre','RS'),
-                   ( '4','4', 'Jonathan','Rua Álvaro Anes', '90', NULL, 'Pinheiros','São Paulo','SP')]
+                   ( '4','4', 'Jonathan','Rua Álvaro Anes', '90', 'NULL', 'Pinheiros','São Paulo','SP')]
 
     cursor.executemany("INSERT INTO cliente( TEL_FIXO, TEL_CEL, NOME_CLI, ENDERECO, NR_END,COMPLEMENTO,BAIRRO,CIDADE,UF) \
                     values (?,?,?,?,?,?,?,?,?)", lista_clientes)
