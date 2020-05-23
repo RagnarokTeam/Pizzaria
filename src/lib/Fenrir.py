2153737f01d132c1cbf47e7c33fba2c4b4f975f3
jeffersonLim <killover8841@gmail.com>
2020-05-22 22:01:04 -0300

    add função CalculaValorFinalPizza

--- src/lib/Fenrir.py
+++ src/lib/Fenrir.py
@@ -2,7 +2,7 @@
 Data........: 2020-05-14
 Projeto.....: RagnarokProject - Pizzaria
 Arquivo.....: Fenrir.py
-Descrição...: Conexão generalizada para toda a aplicação
+Descrição...: Desacorrentamento generalizada para toda a aplicação
 Autor.......: Jefferson de L. Matos - Eq. Ragnarok
 Observações.: 2020-05-22 - [R00] Criação do Arquivo - Versao 1.00
               ...
@@ -12,52 +12,63 @@ from datetime import datetime
 from src.db.Asgard import Bifrost
 
 
-print(numpy)
 def GETDATE():
     data = datetime.now().strftime("%d/%m/%Y")
     return data
 
-#tam = tamanho da pizza
-#nomePizza = nome da pizza
-#meia = é 1/2 pizza? passe 1.5 é inteira? passe 1.
-
-#em casos de ERRO ela vai retornar 1, caso tenha sucesso retorna o valor da pizza de acordo
-#com a documentacao
-
-def CalculaTamPizza(Tam, nomePizza, meia):
+def selectValorPadraoPizza(Tamanho,nomePizza):
     # inicialmente vou usar apenas por nome mas o correto é se usar o cod
     cursor = Bifrost.connection.cursor()
-    SQL = ('SELECT CODIGO_PIZ,NOME_PIZ, VALOR_CUSTO '
-           'FROM PIZZA'
-           'WHERE NOME_PIZ LIKE ? AND DATA_INATIVACAO IS NULL'
-           )
+
     cursor.execute('SELECT DISTINCT CODIGO_PIZ,NOME_PIZ, VALOR_CUSTO FROM PIZZA '
                    'WHERE DATA_INATIVACAO IS NULL'
                    ' AND NOME_PIZ LIKE ?', ('%' + nomePizza + '%',))
+
     data = cursor.fetchone()
 
-    #verificando qual a porcentagem usar
-
+    # verificando qual a porcentagem usar
     if data is not None:
         valorPadrao = data[2]
-        if Tam == 'grande':
+        if Tamanho == 'grande':
             valorPadrao = valorPadrao * 1.35
-        elif Tam == 'media':
+        elif Tamanho == 'media':
             valorPadrao = valorPadrao * 1.25
         else:
             valorPadrao = valorPadrao * 1.15
 
-    else:
-        print('parametro inválido ou sem registro! - erro 00')
-        return 1
-    print(valorPadrao)
-
-    #vefifica se é meia
-
-    if meia != 1:
-
-
 
     return valorPadrao
 
-#CalculaTamPizza('media', 'aleo', 1)
+#tam = tamanho da pizza
+#nomePizza = nome da pizza
+#meia = é 1/2 pizza? passe 1 é inteira? passe 0. só passe se for meia pizza;
+#nomePizza2 = necessario se passar meia.
+#em casos de ERRO ela vai retornar 1, caso tenha sucesso retorna o valor da pizza de acordo
+#com a documentacao
+
+
+def CalculaValorFinalPizza(Tamanho, nomePizza, meia = 0, nomePizza2 = 'null' ):
+
+    valorPizza = valorPadrao1 = float(selectValorPadraoPizza(Tamanho,nomePizza))
+
+    if meia != 0:
+
+        valorPadrao1 = valorPadrao1/2
+        valorPadrao2 = float(selectValorPadraoPizza(Tamanho,nomePizza2))/2
+
+        if valorPadrao1 > valorPadrao2:
+            valorPizza = valorPadrao1 *2
+        else:
+            valorPizza = valorPadrao2 *2
+
+    return valorPizza
+
+
+
+
+
+
+
+
+
+CalculaValorFinalPizza('media', 'aleo', 1,'PERUANA')
