f840d727a6156aa9a58f2e1f3991039474b557fc
paulakemi <paulakemitanaka@hotmail.com>
2020-05-23 22:51:16 -0300

    atualizacao de relatorio

--- src/lib/Relatorios/DadosCliente.py
+++ src/lib/Relatorios/DadosCliente.py
@@ -19,18 +19,18 @@ cursor = Bifrost.connection.cursor()
 cursor.execute("select CODIGO_CLI,TEL_FIXO,TEL_CEL,NOME_CLI,ENDERECO,NR_END,COMPLEMENTO,BAIRRO,CIDADE,UF,DATA_CADASTRO, DATA_INATIVO from cliente")
 user1 = cursor.fetchone()  # retrieve the first row
 print('\nImpressao de campo a campo')
-print('Codigo Cliente:', CODIGO_CLI[0])  # Imprime o primeiro campo
-print('Telefone:', TEL_FIXO[1])  # Imprime o segundo campo
-print('Celular:', TEL_CEL[2])  # Imprime o terceiro campo
-print('Nome:', NOME_CLI[3])  # Imprime o terceiro campo
-print('Endereço:', ENDERECO[4])  # Imprime o quarto campo
-print('Número:', NR_END[5])  # Imprime o quarto campo
-print('Complemento:', COMPLEMENTO[6])  # Imprime o quarto campo
-print('Bairro:', BAIRRO[7])  # Imprime o quarto campo
-print('Cidade:', CIDADE[8])  # Imprime o quarto campo
-print('UF:', UF[9])  # Imprime o quarto campo
-print('Data Cadastro:', DATA_CADASTRO[10])  # Imprime o quarto campo
-print('Data Inativo:', DATA_INATIVO[11])  # Imprime o quarto campo
+print('Codigo Cliente:', user1[0])  # Imprime o primeiro campo
+print('Telefone:', user1[1])  # Imprime o segundo campo
+print('Celular:', user1[2])  # Imprime o terceiro campo
+print('Nome:', user1[3])  # Imprime o terceiro campo
+print('Endereço:', user1[4])  # Imprime o quarto campo
+print('Número:', user1[5])  # Imprime o quarto campo
+print('Complemento:', user1[6])  # Imprime o quarto campo
+print('Bairro:', user1[7])  # Imprime o quarto campo
+print('Cidade:', user1[8])  # Imprime o quarto campo
+print('UF:', user1[9])  # Imprime o quarto campo
+print('Data Cadastro:', user1[10])  # Imprime o quarto campo
+print('Data Inativo:', user1[11])  # Imprime o quarto campo
 
 print('\nImpressao de toda as tuplas')
 all_rows = cursor.fetchall()
@@ -38,6 +38,7 @@ for row in all_rows:
     # row[0] retorna a primeira coluna da query (id),
     # row[1] retorna a segunda coluna (nome)
     print('{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11}'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
+    print('Total de Clientes: '.format(all_rows))
 
 
 # fim da bifrost
--- src/lib/Relatorios/RelatorioClientes.py
+++ src/lib/Relatorios/RelatorioClientes.py
@@ -19,10 +19,10 @@ cursor = Bifrost.connection.cursor()
 cursor.execute("select CODIGO_CLI, NOME_CLI, DATA_PED, sum(b.CODIGO_CLI) AS TOTAL from cliente a inner join pedido b on a.CODIGO_CLI = b.CODIGO_CLI order by data_ped")
 user1 = cursor.fetchone()  # retrieve the first row
 print('\nImpressao de campo a campo')
-print('CODIGO_CLI:', CODIGO_CLI[0])  # Imprime o primeiro campo
-print('NOME_CLI:', NOME_CLI[1])  # Imprime o segundo campo
-print('DATA_PED:', DATA_PED[2])  # Imprime o terceiro campo
-print('TOTAL:', TOTAL[3])  # Imprime o quarto campo
+print('CODIGO_CLI:', user1[0])  # Imprime o primeiro campo
+print('NOME_CLI:', user1[1])  # Imprime o segundo campo
+print('DATA_PED:', user1[2])  # Imprime o terceiro campo
+print('TOTAL:', user1[3])  # Imprime o quarto campo
 (number_of_rows,)=cursor.fetchone()
 
 print('\nImpressao de toda as tuplas')
