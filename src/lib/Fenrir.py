a4a15d1874f596330b111dad5d7ca254b3f02ee9
Camila <cahmilex@users.noreply.github.com>
2020-06-01 15:07:54 -0300

    Relatório por data

--- src/lib/Fenrir.py
+++ src/lib/Fenrir.py
@@ -5,8 +5,8 @@ Arquivo.....: Fenrir.py
 Descrição...: Desacorrentamento generalizada para toda a aplicação utilizando função
 Autor.......: Jefferson de L. Matos - Eq. Ragnarok
 Observações.: 2020-05-22 - [R00] Criação do Arquivo - Versao 1.00
-              ...
-Referencias: imcorporando funções cliente criado pela camila
+              Todas as Funções foram incorporadas ao Fenrir
+Referencias: Imcorporando funções cliente criado pela Camila
 """
 from datetime import datetime,timedelta
 from src.db.Asgard import Bifrost
@@ -141,6 +141,7 @@ def todosRelatorios():
         print('[2] - Detalhamentos clientes')
         print('[3] - Pizzas cadastradas')
         print('[4] - Quantidade de pedidos por cliente')
+        print('[5] - Pedidos Entre Duas Datas')
         print('[6] - Voltar')
         opcaoRelat = eval(input('Digite qual relatório você quer abrir: '))
 
@@ -231,6 +232,23 @@ def todosRelatorios():
                                                                                                              row[2],
                                                                                                              row[3]))
                 print('\n\n')
+            pause()
+            limparTelaOS()
+        elif opcaoRelat == 5: #Fizemos vários testes com as datas, ordem inversa, AAAA/MM/DD, MM/DD/AAAA e mesmo assim não funcionou
+            datainicial = input("Digite a data inicial (formato DD-MM-AAAA)")
+            datafinal = input("Digite a data final (formato DD-MM-AAAA)") 
+            cursor.execute("select COD_PED, DATA_PED, CODIGO_CLI, TOTAL_PED from PEDIDO WHERE DATA_PED BETWEEN "+ datainicial + " AND "+ datafinal)
+        
+            print('\nRELATÓRIO DE PEDIDOS ENTRE', datainicial, 'E', datafinal)
+            all_rows = cursor.fetchall()
+            for user1 in all_rows:
+                print('------------------------------------------------------------')
+                print('Códog do Pedido:', user1[0])  # Imprime o primeiro campo
+                print('Data:', user1[1])  # Imprime o segundo campo
+                print('Códogo do Cliente:', user1[2])  # Imprime o terceiro campo22-03-2020
+                print('Total do Pedido:', user1[3])  # Imprime o terceiro campo
+                print('\n')
+            cursor.connection.commit()    
             pause()
             limparTelaOS()
         else:
